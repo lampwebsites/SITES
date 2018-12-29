728d0f1b27a7a7048065d38328a1761961e13f11
lampwebsites <lampwebsites@gmail.com>
2018-12-29 09:18:19 -0300

    Initial commit

---
+++ bs4wp/layout/gulpfile.js
@@ -0,0 +1,32 @@
+// Abaixo com a versão Gulp 4
+//Criando variaveis
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', gulp.series( function() {
+    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
+    .pipe(sass()) // converter o Sass em CSS
+    .pipe(gulp.dest('src/css'));
+}));
+
+gulp.task('watch', gulp.series( function() {
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], gulp.parallel( ['sass'] ));
+}));
+
+gulp.task('default', gulp.series( ['sass', 'watch'] ));
+
+
+// Abaixo com a versão Gulp 3
+/*
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
+    .pipe(sass()) // converter o Sass em CSS
+    .pipe(gulp.dest('src/css'));
+});
+gulp.task('watch', function() {
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], ['sass'] );
+});
+gulp.task('default', ['sass', 'watch'] );
+*/
